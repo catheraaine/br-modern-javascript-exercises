6d07afbcd956855a387244c748787ac3c9933372
catheraaine <catheraaine@gmail.com>
2017-03-28 12:26:08 -0400

    feat: Exercises 1-4

--- es6-skills/02-const.js
+++ es6-skills/02-const.js
@@ -1,11 +1,13 @@
 /* Use `const` so `x` can't change
- * 
+ *
  * Consider renaming to const convention
  * */
-var x = 1; 
+const x = 1;
 
 if( true ) {
-  var x = 2; // should raise a TypeError
+  let x = 2; // should raise a TypeError
+
+console.log(x); // 2
 }
 
 console.log(x); // 1
